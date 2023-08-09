3814c81155faf6f46754287fa572d99129b812f4
Krzysztof <krzysztof.podmokly@gmail.com>
2023-08-09 22:01:47 +0200

    Initializing Google and Github auth with next-auth

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth/next";
+import type { NextAuthOptions } from "next-auth";
+import GithubProvider from 'next-auth/providers/github'
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string
+    })
+  ]
+}
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
